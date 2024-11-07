7a76131e95f86865d6544308af99e35b780c1903
synderis <toccidylan@gmail.com>
2024-11-07 00:45:50 -0500

    cleaning syntax

--- backend/card_scraper.py
+++ backend/card_scraper.py
@@ -144,7 +144,7 @@ def card_finder(source_df):
         card_types = {'foil': foil, 'reverse-holo': reverse_holo, '1st-edition': first_edition, 'variant': variant}
         
         for type_value in card_types.keys():
-            if (card_id != '' or card_id is not None) and type_value == 'foil' and card_types[type_value] == True:
+            if card_id and type_value == 'foil' and card_types[type_value] == True:
                 card_type = 'holo'
             elif type_value == 'variant' and variant_type in ['', None]:
                 card_type = variant_type
