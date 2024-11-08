df8b6e3eb3bb9fd494b46fb892f79ded317bf72f
raylu <90059+raylu@users.noreply.github.com>
2024-11-07 20:55:40 -0800

    misc no-op fixes

--- backend/ocr_ml_reader.py
+++ backend/ocr_ml_reader.py
@@ -1,3 +1,4 @@
+import functools
 from io import BytesIO
 from PIL import Image, ImageEnhance
 import base64
@@ -8,18 +9,18 @@ import json
 # Register the HEIF opener to enable HEIC format support
 pillow_heif.register_heif_opener()
 
-def read_google_credentials():
+@functools.cache
+def read_google_credentials() -> dict:
     with open('google_info.json', 'r') as file:
         google_credentials = json.load(file)
     return google_credentials
 
-def decode_base64_image(base64_str):
+def decode_base64_image(base64_str: str) -> bytes:
     """Decode a base64 image string to an image file in memory."""
     base64_str = base64_str.split(',')[1]  # Remove the base64 prefix
     # Decode the base64 string
     image_data = base64.b64decode(base64_str)
-    image = Image.open(BytesIO(image_data))
-    
+    image: Image.Image = Image.open(BytesIO(image_data))
     image = image.convert('L')
 
     # Enhance brightness (1.0 = original)
@@ -29,21 +30,18 @@ def decode_base64_image(base64_str):
     # Save the processed image to a BytesIO object
     temp_image_byte_arr = BytesIO()
     image.save(temp_image_byte_arr, format='JPEG')
-    temp_image_byte_arr.seek(0)  # Reset the stream's position to the beginning
-
-    return temp_image_byte_arr
+    return temp_image_byte_arr.getvalue()
 
 
-def detect_card_details(test_image_path):
-    
+def detect_card_details(test_image_base64: str) -> dict[str, str | bool | None]:
     google_credentials_info = read_google_credentials()
     google_vision_client = vision.ImageAnnotatorClient.from_service_account_info(google_credentials_info)
     
     # Process image without saving to disk
-    img_byte_arr = decode_base64_image(test_image_path)
+    img_byte_arr = decode_base64_image(test_image_base64)
     
     # Create an Image object for Vision API from the byte stream
-    image = vision.Image(content=img_byte_arr.read())
+    image = vision.Image(content=img_byte_arr)
 
     # Perform text detection
     response = google_vision_client.document_text_detection(image=image)
@@ -91,13 +89,8 @@ def detect_card_details(test_image_path):
         if any('[' in item for item in lines):
             if test_id_rows_1:
                 card_id = test_id_rows_1[-1].strip()
-            else:
-                card_id = None
-        else:
-            if test_id_rows_2:
+        elif test_id_rows_2:
                 card_id = test_id_rows_2[-1].split('/')[0].split(' ')[-1]
-            else:
-                card_id = None
 
         # Print or store the desired details
         print(f'Card Name: {card_name}')
